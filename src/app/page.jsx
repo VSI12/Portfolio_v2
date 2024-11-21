d58bde965197a2f1c3e41ac167c6c742cd3298eb
Victor Iliya <VictorIliya15@gmail.com>
2024-11-21 11:01:26 +0100

    added the hero content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ViewCounter from "@/components/counter/Counter";
 
 export default function Home() {
   return (
-    <div>
-     page
+    <div className={styles.container}>
+      <p className={styles.title}>
+        Hello, my name is
+      </p>
+      <h1 className={styles.name}>Victor Iliya</h1>
+      <h1 className={styles.name}>I build solutions for the cloud</h1>
+      <p className={styles.description}>
+        I’m a cloud engineer passionate about creating highly available and <br /> 
+        secure cloud solutions. Currently, I’m expanding my skill set to transition into DevOps, <br />
+         focusing on workflow automation, optimized deployments, <br />
+          and enhanced team collaboration for reliable software delivery.
+        </p>
     </div>
   );
 }
