c45fe4aba7308b9b0b10298e0877bd5d9584a3c1
Victor Iliya <VictorIliya15@gmail.com>
2024-11-21 10:13:02 +0100

    Added the links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import styles from "./footer.module.css";
-import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faDev } from "@fortawesome/free-brands-svg-icons";
+import { faDev, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-      <FontAwesomeIcon icon={faGithub} />
-      <FontAwesomeIcon icon={faLinkedin} />
-      <FontAwesomeIcon icon={faDev} />
+      <a href="https://github.com/VSI12" target="_blank" rel="noopener noreferrer" >
+        <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+      </a>
+      <a href="https://dev.to/yourprofile" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faDev} className={styles.icon} />
+      </a>
+      <a href="https://www.linkedin.com/in/victor-iliya/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+      </a>
       </div>
     </div>
   );
