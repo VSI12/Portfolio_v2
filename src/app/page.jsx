bcedb0b53897452ce8d923fad1c7e11bd9251334
Victor Iliya <VictorIliya15@gmail.com>
2024-11-21 17:38:38 +0100

    style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <p className={styles.title}>
-        Hello, my name is
+        Hi, My name is
       </p>
-      <h1 className={styles.name}>Victor Iliya</h1>
-      <h1 className={styles.name}>I build solutions for the cloud</h1>
+      <h1 className={styles.name}>Victor Iliya.</h1>
+      <h1 className={styles.work}>I build solutions for the cloud</h1>
       <p className={styles.description}>
         I’m a cloud engineer passionate about creating highly available and <br /> 
         secure cloud solutions. Currently, I’m expanding my skill set to transition into DevOps, <br />
-         focusing on workflow automation, optimized deployments, <br />
-          and enhanced team collaboration for reliable software delivery.
+        focusing on workflow automation, optimized deployments, <br />
+        and enhanced team collaboration for reliable software delivery.
         </p>
     </div>
   );
