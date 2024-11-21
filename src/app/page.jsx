583ddf133e2320eecebae3bdde19e4aab38eb57d
Victor Iliya <VictorIliya15@gmail.com>
2024-11-21 21:29:29 +0100

    added experience to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import ViewCounter from "@/components/counter/Counter";
 import Hero from "./home/page";
+import Experiences from "./experiences/page";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Hero />
+      <Experiences/>
     </div>
   );
 }
