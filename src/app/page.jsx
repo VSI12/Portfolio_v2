c63147417ed2398bc22eb64916839a28739adbb2
Victor Iliya <VictorIliya15@gmail.com>
2024-11-22 05:21:36 +0100

    hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import ViewCounter from "@/components/counter/Counter";
-import Hero from "./home/page";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Hero />
+      <Hero/>
     </div>
   );
 }
