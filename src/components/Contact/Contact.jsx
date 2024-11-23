28d7802fcaf1b6c1e62318e1f1d858f9a7fc0648
Victor Iliya <VictorIliya15@gmail.com>
2024-11-23 22:30:31 +0100

    first contact change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import styles from './Contact.module.css'
+import styles from './contact.module.css'
 
 const Contact = () => {
   return (
-      <section className={styles.section} id="contact">
-        <h2 className={styles.title}>Contact</h2>
+      <section className={`${styles.contact} ${styles.section}`} id="contact">
+        <h2 className={styles.section__title}>Contact Me</h2>
+        <span className={styles.section__subtitle}>Let&#39;s Get In Touch</span>
+        <div className={styles.contact__container}>
+          <div className={styles.contact__content}>
+            I&#39;m always open to new challenges and interesting <br />
+            projects. If you have an opportunity, a question, or <br />
+            just want to say hello, feel free to drop me a message. <br />
+             I&#39;ll do my best to respond quickly!
+          </div>
+          <div className={styles.contact__button}>
+
+          </div>
+        </div>
       </section>
   )
 }
