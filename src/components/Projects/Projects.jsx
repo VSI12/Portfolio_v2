8b1555121cc5c4de2d055da3882cfb78b3b2e1c9
Victor Iliya <VictorIliya15@gmail.com>
2024-11-25 21:54:19 +0100

    over lapped

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import styles from './Projects.module.css'
+import styles from './projects.module.css'
+import Image from 'next/image'
 const Projects = () => {
   return (
     <section className={styles.section} id="projects">
-      <h2 className={styles.title}>Projects</h2>
-    </section>
+        <h2 className={styles.section__title}>Projects</h2>
+        <span className={styles.section__subtitle}>Featured builds</span>
+
+        <div className={`${styles.projects__container} ${styles.container} ${styles.grid}`}>
+          <div className={styles.projects__card}>
+            <div className={styles.projects__image}>
+              <Image src="/IDS-Architecture.png" alt="AWS Architecture" width={500} height={354} />
+            </div>
+
+            <div className={styles.projects__data}>
+            A proof of concept flask web-app based on the NSL-Kdd dataset for network data calssification
+            Users upload network traffic data files, and the app processes and classifies this data to detect potential security threats or anomalies
+            </div>
+          </div>
+        </div>
+      </section>
   )
 }
 
