0ee90af4965370c703696d1becb8434c19750799
Victor Iliya <VictorIliya15@gmail.com>
2024-11-29 12:41:22 +0100

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,6 +7,20 @@ const Footer = () => {
   return (
     <div className={styles.footer__container}>
       <span className={styles.footer}>Designed and built by Victor Iliya</span>
+      <div className={styles.floatingWidget}>
+        <div className={styles.social}>
+          <a href="https://github.com/VSI12" target="_blank" rel="noopener noreferrer" >
+            <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+          </a>
+          <a href="https://dev.to/non-existent" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faDev} className={styles.icon} />
+          </a>
+          <a href="https://www.linkedin.com/in/victor-iliya/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+          </a>
+        </div>
+    </div>
+      
       <span className={styles.footer__copy}>&#169; Victor Iliya. All right reserved. 2024</span>
     </div>
   );
