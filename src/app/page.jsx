ad34b5806aeec141185a46c140f1c952a4c75841
Victor Iliya <VictorIliya15@gmail.com>
2024-12-05 14:46:56 +0100

    restructed the site

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,12 @@
 import styles from "./page.module.css";
 import Hero from "@/components/Hero/Hero";
-import About from "@/components/About/About";
 import Socials from "@/components/Socials/Socials";
-import Contact from "@/components/Contact/Contact";
-import Projects from "@/components/Projects/Projects";
-import Experiences from "@/components/Experiences/Experiences";
+
 
 export default function Home() {
   return (
     <div>
       <Hero/>
-      <About/>
-      <Projects/>
-      <Experiences/>
-      <Contact/>
       <Socials/>
     </div>
   );
